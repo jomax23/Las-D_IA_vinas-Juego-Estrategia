a86714f9a415f0d63256444599e04c85e7d62621
José María <josemariamca8@gmail.com>
2024-11-06 17:45:05 +0100

    cambios assets y mapa

--- Assets/Scripts/FieldObstacleGeneration.cs
+++ Assets/Scripts/FieldObstacleGeneration.cs
@@ -6,21 +6,47 @@ public class FieldObstacleGeneration : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public int numColumnas = 13;
+    public int numColumnas = 11;
     public int numFilas = 7;
 
     public List<Tile> listOfTiles = new List<Tile>();
 
     int randObstacleIndex;
+    public List<int> usedObstacleIndex = new List<int>();
+    bool isUsed = false;
 
     public int maxObstacleAmount = 3;
     void Awake()
     {
-
         for (int i = 0; i < maxObstacleAmount; i++)
         {
-            randObstacleIndex = Random.Range(0, numColumnas * numFilas);
+            randObstacleIndex = Random.Range(0, listOfTiles.Count);
 
+            if(usedObstacleIndex.Count == 0)
+            {
+                usedObstacleIndex.Add(randObstacleIndex);
+                usedObstacleIndex.Add(randObstacleIndex + 1);
+                usedObstacleIndex.Add(randObstacleIndex - 1);
+            }
+            else
+            {
+                while (isUsed)
+                {
+                    if (!usedObstacleIndex.Contains(randObstacleIndex))
+                    {
+                        usedObstacleIndex.Add(randObstacleIndex);
+                        usedObstacleIndex.Add(randObstacleIndex + 1);
+                        usedObstacleIndex.Add(randObstacleIndex - 1);
+                        isUsed = false;
+                    }
+                    else
+                    {
+                        randObstacleIndex = Random.Range(0, listOfTiles.Count);
+                    }
+                }
+                isUsed = true;
+                
+            }
             if (!listOfTiles[randObstacleIndex].obstaculo.activeSelf)
             {
                 listOfTiles[randObstacleIndex].obstaculo.SetActive(true);
@@ -30,6 +56,11 @@ public class FieldObstacleGeneration : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+
+    }
+
+    void SeeIfUsedObstacleIndex()
     {
 
     }
